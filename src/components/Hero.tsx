565a5c504d107c4d5ca659fe4efa0f2a848cd12b
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-15 15:14:14 -0700

    style: Hero Component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,11 +6,10 @@ import ArrowIcon from "../assets/icons/arrow-right.svg";
 import Hand1 from "../assets/images/hand1.png";
 import Hand2 from "../assets/images/hand2.png";
 
-
 export default function Hero() {
   return (
     <div className="relative text-white py-[72px] sm:py-24 bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] overflow-clip">
-      <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2400px] lg:h-[1200px] rounded-[100%] left-1/2 -translate-x-1/2 bg-black border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]" />
+      <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2400px] lg:h-[1200px] rounded-[100%] left-1/2 -translate-x-1/2 bg-black bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]" />
       <div className="container relative">
         <div className="flex items-center justify-center">
           <a
@@ -33,19 +32,33 @@ export default function Hero() {
               <br />
               at a Time
             </h1>
-            <Image src={Hand1} className="hidden sm:inline absolute right-[476px] top-[108px]" height="200" width="200" alt="Hand1"  />
-            <Image src={Hand2} className="hidden sm:inline absolute top-[56px] left-[498px]" height="200" width="200" alt="Hand2"  />
+            <Image
+              src={Hand1}
+              className="hidden sm:inline absolute right-[476px] top-[108px]"
+              height="200"
+              width="200"
+              alt="Hand1"
+            />
+            <Image
+              src={Hand2}
+              className="hidden sm:inline absolute top-[56px] left-[498px]"
+              height="200"
+              width="200"
+              alt="Hand2"
+            />
           </div>
         </div>
         <div className="flex justify-center">
-          <p className="text-center text-xl tracking-tight mt-8 max-w-md">
-            Celebrate the joy of accomplishment with an app designed to track
-            progress, motivate your efforts, and Celebrate your successes.
+          <p className="text-center text-xl tracking-tight mt-8 max-w-md text-white/80">
+            Seamaless design subscriptions, No hiring, no micromanagenent, no hassle
           </p>
         </div>
         <div className="flex items-center justify-center gap-4 mt-8">
-          <button className="bg-white text-black py-3 px-5 rounded-lg font-medium">
-            Get for Free
+          {/* <button className="bg-white text-black py-3 px-5 rounded-lg font-medium">
+            Get Started
+          </button> */}
+          <button className="bg-white text-black font-medium py-3 px-5 rounded-full shadow-md transform transition-transform duration-300 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-purple-300 hover:shadow-purple-500/50">
+            Glowing Button
           </button>
         </div>
       </div>
